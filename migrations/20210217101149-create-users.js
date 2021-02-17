c91b812f4fe1a43f160fcbcac5eed6c5c809dd0d
azemoning <ahmadsaufimaulana@gmail.com>
2021-02-17 17:14:11 +0700

    create new Users migration

---
+++ migrations/20210217101149-create-users.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: false,
+        primaryKey: true,
+        type: Sequelize.STRING(22)
+      },
+      username: {
+        type: Sequelize.STRING(18)
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
