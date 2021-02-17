c9666b6db228604013aae68df45cb4c76083d751
azemoning <ahmadsaufimaulana@gmail.com>
2021-02-17 17:18:22 +0700

    add authentication route

---
+++ routes/authRoute.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express.Router()
+
+app.get('/', async (req, res) => {
+    res.send()
+})
+
+app.post('/', async (req, res) => {
+    res.send()
+})
+
+app.put('/:id', async (req, res) => {
+    res.send()
+})
+
+app.delete('/:id', async (req, res) => {
+    res.send()
+})
